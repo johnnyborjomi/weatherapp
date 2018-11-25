a07ee286ff4365e1ed242a6fed7608ad8c6dcd05
bobanko <mishko.vladimir@gmail.com>
2018-11-25 23:09:59 +0200

    shitty babel + async configured

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,57 +1,53 @@
-import { throttle } from "lodash"
+import { throttle } from "lodash";
 
 import * as React from "react";
 
-import ApiGetter from './api-getter';
+import ApiGetter from "./api-getter";
 
 const test = new ApiGetter();
-test.getResponse('Lviv');
+test.getResponse("Lviv");
 
 export class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            cityName: null,
-            weatherData: {}
-        };
-
-        this.ApiResoponseGetter = new ApiGetter();
-
-        this.citySelect = document.getElementById('select');
+    this.state = {
+      cityName: null,
+      weatherData: {}
     };
 
-    getWeather(event) {
-        console.log(event.target.value);
-        let cityName = event.target.value;
-        this.setState({ cityName });
-        this.ApiResoponseGetter.getResponse(cityName).then(weatherData => {
-            this.setState({ weatherData });
-        });
+    this.ApiResoponseGetter = new ApiGetter();
 
+    this.citySelect = document.getElementById("select");
+  }
 
-    }
+  async getWeather(event) {
+    console.log(event.target.value);
+    let cityName = event.target.value;
+    this.setState({ cityName });
+    let weatherData = await this.ApiResoponseGetter.getResponse(cityName);
 
-    render() {
+    this.setState({ weatherData });
+  }
 
-        let { cityName, temp, weatherData } = this.state;
+  render() {
+    let { cityName, temp, weatherData } = this.state;
 
-        return (
-            <div className="screen">
-                <span>Select City</span>
-                <hr />
-                <select id="select" onChange={(event) => this.getWeather(event)}>
-                    <option disabled>Select City</option>
-                    <option value="Kharkiv">Kharkiv</option>
-                    <option value="Kiev">Kiev</option>
-                    <option value="Lviv">Lviv</option>
-                </select>
-                <div className="results">
-                    <span>{cityName}</span>
-                    <span>{JSON.stringify(weatherData)}</span>
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="screen">
+        <span>Select City</span>
+        <hr />
+        <select id="select" onChange={event => this.getWeather(event)}>
+          <option disabled>Select City</option>
+          <option value="Kharkiv">Kharkiv</option>
+          <option value="Kiev">Kiev</option>
+          <option value="Lviv">Lviv</option>
+        </select>
+        <div className="results">
+          <span>{cityName}</span>
+          <span>{JSON.stringify(weatherData)}</span>
+        </div>
+      </div>
+    );
+  }
 }
