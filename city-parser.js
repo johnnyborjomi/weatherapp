8625a5248396ec5fec64844bacaa3d6a115c4cdb
johnnyborjomi <johnnyborjomi@gmail.com>
2019-07-10 20:58:55 +0300

    us cities added

--- city-parser.js
+++ city-parser.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
 
-const citiesText = fs.readFileSync('./src/assets/city.list.json');
+// const citiesText = fs.readFileSync('./src/assets/city.list.json');
+const citiesText = fs.readFileSync('./src/assets/usaCities.js');
 const cities = JSON.parse(citiesText);
 
 console.log(cities[1]);
 
 
-let mappedCities = cities.filter((city) => {
-    if (city.country == "UA") return city.name
-}).map(city => city.name);
+// let mappedCities = cities.filter((city) => {
+//     if (city.country == "UA") return city.name
+// }).map(city => city.name);
+
+
+let mappedCities = cities.map(city => city.city);
+
 
 function removeDuplicates(arr) {
     return [...new Set(arr)];
@@ -17,4 +22,4 @@ mappedCities = removeDuplicates(mappedCities);
 
 console.log(mappedCities[1], mappedCities.length);
 
-fs.writeFileSync('./src/assets/mappedCityList.js', JSON.stringify(mappedCities));
+fs.writeFileSync('./src/assets/mappedCityList_us.js', JSON.stringify(mappedCities));
